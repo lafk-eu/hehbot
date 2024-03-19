984a52e19a88dc52c3c93ab2e925e4bbd08f82c7
setbe <mrmiquy@gmail.com>
2024-03-20 01:29:30 +0200

    save

--- hehbot/key_api.py
+++ hehbot/key_api.py
@@ -1,14 +1,12 @@
+import os
+
 class APIHolder:
     def __init__(self):
-        self.bot = self.read_api_key("bot_key.txt")
-        self.chatgpt = self.read_api_key("chatgpt_key.txt")
+        self.bot = self.read_api_from_env("TG_BOT_API")
+        self.chatgpt = self.read_api_from_env("OPENAI_API")
 
-    def read_api_key(self, filename):
-        try:
-            with open(filename, "r") as file:
-                return file.read().strip()
-        except FileNotFoundError:
-            print(f"Файл {filename} не знайдено. Будь ласка, створіть файл та помістіть туди ключ API.")
-            return ""
+    def read_api_from_env(self, env_variable):
+        return os.getenv(env_variable, "")
     
 api = APIHolder()
+    
